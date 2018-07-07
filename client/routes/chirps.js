8ff7b0a00768f83f9b3436399d8081088515400d
Warryn Evins <W.Hatmaker@yahoo.com>
2018-07-07 14:54:04 -0500

    updated api methods
    added the put and delete instances, need to finish buildin g them

--- client/routes/chirps.js
+++ client/routes/chirps.js
@@ -5,15 +5,26 @@ let router = express.Router();
 
 router.get('/:id?', (req, res) => {
     let id = req.params.id
+
     if(id) {
-        res.json(chirpStore.GetChirp(req));
+        res.send(chirpStore.GetChirp(id));
     } else {
-    res.send(chirpStore.GetChirp());
+        res.send(chirpStore.GetChirps());
     }
 });
+
 router.post('/', (req, res) => {
     chirpStore.CreateChirp(req.body);
     res.sendStatus(200);
-}
-)
+});
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    // TODO: Handle updating of specified record
+});
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    // TODO: handle delete of specified record
+});
 module.exports = router;
